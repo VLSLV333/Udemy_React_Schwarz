42b0c64a65714104dd07bc5d2427bf646a0c6165
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-02-16 12:02:07 +0200

    Header is done. Only visual part of it, to be fair. Logic needs to be done!

--- tasks/01-food-order-app/src/Components/Header/Header.js
+++ tasks/01-food-order-app/src/Components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import Button from "../UI/Button/Button";
 import style from './Header.module.css'
+import cartSVG from '../icons/cart-shopping-solid.svg'
 
 const Header = props => {
+
     const dummyOrders = 1
 
     return (
-        <header className="background={ the food photo on the video }">
+        <header className={style.back}>
         <div className={style.red}>
           <h2>ReactMeals</h2>
-          <Button className={style.button}>Your cart {dummyOrders}</Button>
+          <Button className={style.button}><img className={style.cart} src ={cartSVG} alt="cart icon"/>Your Cart <span className={style.orders}>{dummyOrders}</span></Button>
         </div>
       </header>
     )
