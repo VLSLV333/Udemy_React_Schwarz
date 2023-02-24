185adf504d68de207c83c590442f1c0f548c4478
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-02-24 22:33:33 +0200

    form validation was practised today! Still not coding enough this days, about 10 days to go and I will be finally back!

--- tasks/01-Forms/src/components/SimpleInput.js
+++ tasks/01-Forms/src/components/SimpleInput.js
@@ -1,59 +1,41 @@
-import { useEffect, useState, useRef } from "react";
+import { useState } from "react";
 
 const SimpleInput = (props) => {
-  const nameRef = useRef("");
+
   // useRef is better in case we want only obtain inputed value on form submit!
 
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
 
-  useEffect(() => {
-    if (enteredNameIsValid) {
-      console.log(
-        "This logs on loading of page, tho user has not provided ANY input yet!"
-      );
-    }
-  }, [enteredNameIsValid]);
+  // provide validation logic
+  const enteredNameIsValid = enteredName.trim() !== '';
+  let formIsValid = false
+
+    if (enteredNameIsValid){
+      formIsValid = true
+    } 
 
   const nameInputChangeHandler = (event) => {
     // useState is better in case we want to obtain inputed value on every key input.
-
-    // every keystroke logic
-    if (enteredNameTouched && event.target.value.trim().length >= 5) {
-      setEnteredNameIsValid(true);
-    }
-
     setEnteredName(event.target.value);
-    // console.log(nameRef.current.value)
   };
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (enteredName.trim().length < 5) {
-      setEnteredNameIsValid(false);
-      return;
-    } else {
-      setEnteredNameIsValid(true);
-    }
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
     setEnteredNameTouched(true);
 
-    if (enteredName.trim().length < 5) {
-      setEnteredNameIsValid(false);
+    if (!enteredNameIsValid) {
       return;
     }
-
-    setEnteredNameIsValid(true);
     setEnteredName("");
+    setEnteredNameTouched(false)
+
     // or a worse way to do it, because we manipulate directly the DOM
     // nameRef.current.value = ''
-
-    console.log(enteredName);
-    console.log(nameRef);
   };
 
   let inputDynamicStyles = enteredNameIsValid
@@ -71,7 +53,6 @@ const SimpleInput = (props) => {
           id="name"
           value={enteredName}
           onChange={nameInputChangeHandler}
-          ref={nameRef}
           onBlur={nameInputBlurHandler}
         />
         {!enteredNameIsValid && enteredNameTouched && (
@@ -79,7 +60,7 @@ const SimpleInput = (props) => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
