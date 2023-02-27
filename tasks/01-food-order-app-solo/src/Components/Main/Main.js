b1313d481b694961313a53274e4ff4c77761ce0b
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-02-27 19:12:06 +0200

    need to learn how to use Firebase 'backend'. That is tommorrow

--- tasks/01-food-order-app-solo/src/Components/Main/Main.js
+++ tasks/01-food-order-app-solo/src/Components/Main/Main.js
@@ -4,37 +4,58 @@ import style from "./Main.module.css";
 import MenuElement from "./MenuElement";
 
 const Main = (props) => {
+
+  const fetchMenu = async () => {
+    try {
+      const responce = await fetch('https://react-learn-http-post-default-rtdb.europe-west1.firebasedatabase.app/menu.json')
+      if (!responce.ok){
+        throw new Error('smth s not working!')
+      }
+      const menuData = await responce.json()
+      // const menuData = responce
+      const test = JSON.parse(menuData)
+      console.log(test)
+      // console.log(JSON.parse(menuData))
+      // for (let food in menuData){
+      //   console.log(menuData)
+      // } 
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  fetchMenu()
+
   const menu = [
     {
       item: "Sushi",
       description: "Finest fish and veggies",
       price: "22.99",
-      id: Math.random().toString()
+      id: Math.random().toString(),
     },
     {
       item: "Schnitzel",
       description: "A german specialty!",
       price: "16.50",
-      id: Math.random().toString()
+      id: Math.random().toString(),
     },
     {
       item: "Barbecue Burger",
       description: "American, raw, meaty",
       price: "12.99",
-      id: Math.random().toString()
+      id: Math.random().toString(),
     },
     {
       item: "Green Bowl",
       description: "Healthy...and green...",
       price: "18.99",
-      id: Math.random().toString()
+      id: Math.random().toString(),
     },
   ];
 
   return (
     <main className={style.main}>
       <Card className={style.banner}>
-        <h1>Delicious Food, Delivered To You</h1>
+        <h1>Delicious Julia, Delivered To You</h1>
         <p>
           Choose your favorite meal from our broad selection of available meals
           and enjoy a delicious lunch or dinner at home.
