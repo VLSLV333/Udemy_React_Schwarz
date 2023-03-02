8749a0416e1b1f9809015817465270c72378b01d
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-02 21:41:37 +0200

    custom action creators for async tasks look not that easy, but still better then custom hooks=)

--- tasks/02-Redux-Async-Code-Http-Requests/src/components/store/index.js
+++ tasks/02-Redux-Async-Code-Http-Requests/src/components/store/index.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 
 import showCartReducer from './showCart'
 import cartOrderReducer from './cartOrder'
-// import menuReducer from './menu'
 
 const store = configureStore({
     reducer: {
         showCart: showCartReducer,
         cartOrder: cartOrderReducer,
-        // menu: menuReducer,
     }
 })
 
