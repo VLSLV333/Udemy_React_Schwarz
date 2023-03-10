50e9fb195f7251609df50e24337ef05b8d6161a2
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-03-10 16:49:06 +0200

    Created logic for hiding Notification component.

--- tasks/02-Redux-Async-Code-Http-Requests/src/components/UI/Notification.js
+++ tasks/02-Redux-Async-Code-Http-Requests/src/components/UI/Notification.js
@@ -1,23 +1,40 @@
 import classes from './Notification.module.css';
 
+import { showCartActions } from '../store/showCart';
+
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+
 const Notification = (props) => {
-  let specialClasses = '';
+	const dispatch = useDispatch();
 
-  if (props.status === 'error') {
-    specialClasses = classes.error;
-  }
-  if (props.status === 'success') {
-    specialClasses = classes.success;
-  }
+	let specialClasses = '';
 
-  const cssClasses = `${classes.notification} ${specialClasses}`;
+	if (props.status === 'error') {
+		specialClasses = classes.error;
+	}
+	if (props.status === 'success') {
+		specialClasses = classes.success;
+	}
 
-  return (
-    <section className={cssClasses}>
-      <h2>{props.title}</h2>
-      <p>{props.message}</p>
-    </section>
-  );
+	const cssClasses = `${classes.notification} ${specialClasses}`;
+
+	useEffect(() => {
+		let test = setTimeout(() => {
+			dispatch(showCartActions.hideNotification());
+		}, 3000);
+
+		return () => {
+			clearTimeout(test);
+		};
+	}, [dispatch, props.status]);
+
+	return (
+		<section className={cssClasses}>
+			<h2>{props.title}</h2>
+			<p>{props.message}</p>
+		</section>
+	);
 };
 
 export default Notification;
