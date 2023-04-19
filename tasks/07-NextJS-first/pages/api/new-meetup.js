145bff55c493b017654811394fb376196173cec6
Vladyslav Sherstiuk <vlad.shrst@gmail.com>
2023-04-19 23:58:08 +0300

    feat: project done. Try to deploy

---
+++ tasks/07-NextJS-first/pages/api/new-meetup.js
@@ -0,0 +1,29 @@
+//  /api/new-meetup
+
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
+	// =======          req.method = alows us to tell which request was sent
+	//  =======         req.body = contais data of incoming request
+
+	if (req.method === 'POST') {
+		const data = req.body;
+
+		const client = await MongoClient.connect(
+			'mongodb+srv://vlad_nextJSapp:vlad_pass123@testnextjsfirstapp.1k7xikz.mongodb.net/meetUp?retryWrites=true&w=majority'
+		);
+		const db = client.db();
+
+		const meetUpsCollection = db.collection('meetupsDB');
+
+		const result = await meetUpsCollection.insertOne(data);
+
+		console.log(result);
+
+		client.close();
+
+		res.status(201).json({ message: 'Meetup inserted!' });
+	}
+}
+
+export default handler;
